5bf5ad4fda03c3e1b23f698d18d0594138955136
Sal Gamez <gamezsalvador@gmail.com>
2020-09-29 18:19:31 -0600

    Added additional variable declaritions and added all confirm prompts for setPassword function

--- script.js
+++ script.js
@@ -7,7 +7,10 @@ var CharTypeSpecial = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "_", "-", "+
 var CharTypeLc = ["a", "b", "c", "d", "e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 var CharTypeUc = ["A", "B", "C", "D", "E", "F", "G", "H","I","J", "K","L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y","Z"]
 var CharTypeNum = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0",]
-
+var confirmCharTypeSpecial ;
+var confirmCharTypeLc;
+var confirmCharTypeUc;
+var confirmCharTypeNum;
 
 // Write password to the #password input
 function writePassword() {
@@ -28,7 +31,13 @@ var PassLength = parseInt(prompt("How many characters would you like your passwo
     alert("Please try a number between 8 and 128.");
     setPassword()
   }
-var SpecialChar = confirm("Would you like include Special Characters in your password?");
+
+// User Confirmation Inputs
+confirmCharTypeSpecial = confirm("Would you like include Special Characters in your password?");
+confirmCharTypeLc = confirm("Would you like to include Lower case Characters in your Password?");
+confirmCharTypeLc = confirm("Would you like to include Upper case Characters in your Password?");
+confirmCharTypeLc = confirm("Would you like to include Numbers in your Password?");
+
 }
 
 // Add event listener to generate button
