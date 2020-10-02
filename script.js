cf61e567d722cd04eedbe72ded94b8f6e9b0fdda
Sal Gamez <gamezsalvador@gmail.com>
2020-10-02 00:00:31 -0600

    Changed the grouping of code in the script. Added math.random t

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = setPassword();
+  var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
@@ -14,66 +14,73 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+function generatePassword(){
 // Variable Declaration
-var CharTypeLength = 0;
-var CharTypeSpecial = "`~!@#$%^&*()_-=+[]{}|;':\",.<>?"
-var CharTypeLc = "abcdefghijklmnopqrstuvwxyz";
-var CharTypeUc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var CharTypeNum = "1234567890";
-var confirmCharTypeSpecial = "";
-var confirmCharTypeLc = "";
-var confirmCharTypeUc = "";
-var confirmCharTypeNum = "";
-var allCharTypes = "";
+var CharTypeUcList = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var CharTypeLcList = "abcdefghijklmnopqrstuvwxyz";
+var CharTypeNumList = "1234567890";
+var CharTypeSpecialList = "`~!@#$%^&*()_-=+[]{}|;':\",.<>?";
+
+
+// Characters Allowed
+var TotalCharTypes = "";
+
+var CharTypeUcConfirm = "";
+var CharTypeLcConfirm = "";
+var CharTypeNumConfirm = "";
+var CharTypeSpecialConfirm = "";
+var ChartTypeIndex = "";
+var CharTypeAdder = "";
+var CharTypePassword = "";
 
 
 
 // Prompt Password Length (8-128 Characters)
-function setPassword() {
+function CharTypeUserInput(){
+TotalCharTypes = "";
 CharTypeLength = parseInt(prompt("How many characters would you like your password to contain?"))
-  if (CharTypeLength < 8 || CharTypeLength > 128){
-    alert("Please try a number between 8 and 128.")
-      setPassword() 
-    console.log(CharTypeLength)
-     }if (isNaN(CharTypeLength)){
-    alert("Please try again, input a number.")
-      setPassword()
-     }else allCharTypes = allCharTypes.concat(CharTypeLength)
-
-// User Confirmation Inputs
-confirmCharTypeSpecial = confirm("Would you like to include Special Characters in your Password?")
-  if(confirmCharTypeSpecial == true){
-    allCharTypes = allCharTypes.concat(CharTypeSpecial);
-  }
-
-confirmCharTypeLc = confirm("Would you like to include Lower case Characters in your Password?")
-  if(confirmCharTypeLc == true){
-    allCharTypes = allCharTypes.concat(confirmCharTypeLc);
-  }
-
-confirmCharTypeUc = confirm("Would you like to include Upper case Characters in your Password?")
-  if(confirmCharTypeUc == true){
-    allCharTypes = allCharTypes.concat(confirmCharTypeUc)
-  }
-
-confirmCharTypeNum = confirm("Would you like to include Numbers in your Password?")
-  if(confirmCharTypeNum == true){
-    allCharTypes = allCharTypes.concat(confirmCharTypeNum)
-  }
- 
-  console.log(CharTypeLength)
-  console.log(confirmCharTypeSpecial)
-  console.log(confirmCharTypeLc)
-  console.log(confirmCharTypeUc)
-  console.log(confirmCharTypeNum)
-  console.log(allCharTypes)
+CharTypeUcConfirm = confirm("Would you like to include Upper case Characters in your Password?")
+CharTypeLcConfirm = confirm("Would you like to include Lower case Characters in your Password?")
+CharTypeNumConfirm = confirm("Would you like to include Numbers in your Password?")
+CharTypeSpecialConfirm = confirm("Would you like to include Special Characters in your Password?")
 }
- 
-// Add event listener to generate button
-
-
-
-setPassword()
-generateBtn.addEventListener("click", writePassword);
   
+
+while (TotalCharTypes === "" || CharTypeLength < 8 || CharTypeLength > 128){
+    CharTypeUserInput();
+// User Confirmation Inputs
+
+if(CharTypeUcConfirm === true){
+  TotalCharTypes = TotalCharTypes + CharTypeUcList;
+}
+
+if(CharTypeLcConfirm === true){
+  TotalCharTypes = TotalCharTypes + CharTypeLcList;
+}
+
+if(CharTypeNumConfirm === true){
+  TotalCharTypes = TotalCharTypes + CharTypeNumList;
+  }
+
+if(CharTypeSpecialConfirm === true){
+  TotalCharTypes = TotalCharTypes + CharTypeSpecialList;
+  }
+
+  if(TotalCharTypes === "" || CharTypeLength < 8 || CharTypeLength > 128){
+    alert("Please enter a number")
+  }
+}
+
+console.log(CharTypePassword)
+
+for(var i = CharTypeLength; i > 0; i--){
+  ChartTypeIndex = Math.floor(Math.random() * TotalCharTypes.length);
+  CharTypeAdder = TotalCharTypes[ChartTypeIndex];
+  CharTypePassword = CharTypePassword + CharTypeAdder;
+ }
+return(CharTypePassword);
+   
+  }
+
+  // generatePassword()
 
