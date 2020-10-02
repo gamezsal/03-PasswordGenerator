98d35ea5622080a9a871976d9d3dd8491b8d40ac
Sal Gamez <gamezsalvador@gmail.com>
2020-10-01 22:04:54 -0600

    Wired up Generate Password button, changed variable logic and

--- script.js
+++ script.js
@@ -1,48 +1,79 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Variable Declaration
-var CharLength = 0;
-var CharTypeSpecial = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "_", "-", "+", "=","|", "(", ")", "{","}", "[","]", ":",";", "<", ">", "?", "/"]
-var CharTypeLc = ["a", "b", "c", "d", "e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-var CharTypeUc = ["A", "B", "C", "D", "E", "F", "G", "H","I","J", "K","L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y","Z"]
-var CharTypeNum = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0",]
-var confirmCharTypeSpecial ;
-var confirmCharTypeLc;
-var confirmCharTypeUc;
-var confirmCharTypeNum;
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = setPassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
 }
 
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+// Variable Declaration
+var CharTypeLength = 0;
+var CharTypeSpecial = "`~!@#$%^&*()_-=+[]{}|;':\",.<>?"
+var CharTypeLc = "abcdefghijklmnopqrstuvwxyz";
+var CharTypeUc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var CharTypeNum = "1234567890";
+var confirmCharTypeSpecial = "";
+var confirmCharTypeLc = "";
+var confirmCharTypeUc = "";
+var confirmCharTypeNum = "";
+var allCharTypes = "";
+
+
+
 // Prompt Password Length (8-128 Characters)
 function setPassword() {
-var PassLength = parseInt(prompt("How many characters would you like your password to contain?"))
-  if (isNaN(PassLength)){
-    alert("Please try again, input a number.");
-    setPassword()
-  } else if (PassLength < 8 || PassLength > 128){
-    alert("Please try a number between 8 and 128.");
-    setPassword()
-  }
+CharTypeLength = parseInt(prompt("How many characters would you like your password to contain?"))
+  if (CharTypeLength < 8 || CharTypeLength > 128){
+    alert("Please try a number between 8 and 128.")
+      setPassword() 
+    console.log(CharTypeLength)
+     }if (isNaN(CharTypeLength)){
+    alert("Please try again, input a number.")
+      setPassword()
+     }else allCharTypes = allCharTypes.concat(CharTypeLength)
 
 // User Confirmation Inputs
-confirmCharTypeSpecial = confirm("Would you like include Special Characters in your password?");
-confirmCharTypeLc = confirm("Would you like to include Lower case Characters in your Password?");
-confirmCharTypeLc = confirm("Would you like to include Upper case Characters in your Password?");
-confirmCharTypeLc = confirm("Would you like to include Numbers in your Password?");
+confirmCharTypeSpecial = confirm("Would you like to include Special Characters in your Password?")
+  if(confirmCharTypeSpecial == true){
+    allCharTypes = allCharTypes.concat(CharTypeSpecial);
+  }
 
+confirmCharTypeLc = confirm("Would you like to include Lower case Characters in your Password?")
+  if(confirmCharTypeLc == true){
+    allCharTypes = allCharTypes.concat(confirmCharTypeLc);
+  }
+
+confirmCharTypeUc = confirm("Would you like to include Upper case Characters in your Password?")
+  if(confirmCharTypeUc == true){
+    allCharTypes = allCharTypes.concat(confirmCharTypeUc)
+  }
+
+confirmCharTypeNum = confirm("Would you like to include Numbers in your Password?")
+  if(confirmCharTypeNum == true){
+    allCharTypes = allCharTypes.concat(confirmCharTypeNum)
+  }
+ 
+  console.log(CharTypeLength)
+  console.log(confirmCharTypeSpecial)
+  console.log(confirmCharTypeLc)
+  console.log(confirmCharTypeUc)
+  console.log(confirmCharTypeNum)
+  console.log(allCharTypes)
 }
-
+ 
 // Add event listener to generate button
 
 
 
 setPassword()
 generateBtn.addEventListener("click", writePassword);
+  
+
